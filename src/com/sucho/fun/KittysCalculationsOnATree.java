54a456c5e266507f6d726c7e68231cbd3e293d64
Sungje Cho <sucho@groupon.com>
2017-06-22 20:47:21 -0700

    BTHeight & another wrong Kitty

--- src/com/sucho/fun/KittysCalculationsOnATree.java
+++ src/com/sucho/fun/KittysCalculationsOnATree.java
@@ -1,7 +1,6 @@
 package com.sucho.fun;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,54 +12,51 @@ import java.util.Scanner;
  */
 public class KittysCalculationsOnATree {
 
-    private static boolean[] visited;
-
-    private static void getPath(List<Integer> path, boolean[][] edges, int from, int to) {
-        visited[from] = true;
-        path.add(from);
-
-        if (visited[to]) {
-            return;
-        }
-
-        for (int i = 1; i < edges.length; i++) {
-            if (!edges[from][i] || visited[i]) {
-                continue;
-            }
-
-            getPath(path, edges, i, to);
-            if (visited[to]) {
-                return;
-            }
-            path.remove(path.size() - 1);
+    static class Node {
+        Node other;
+        int data;
+        Node(int data) {
+            this.data = data;
         }
     }
 
-    private static int getDist(List<Integer> path1, List<Integer> path2) {
-        int i = 0;
-        for (; i < path1.size() && i < path2.size(); i++) {
-            if (!path1.get(i).equals(path2.get(i))) {
+    private static Node[] nodes;
+
+    private static long getDist(List<Node> path1, List<Node> path2) {
+        long size1 = path1.size();
+        long size2 = path2.size();
+        long i = 0;
+        for (; i < size1 && i < size2; i++) {
+            if (path1.get((int) (size1 - i - 1)).data != path2.get((int) (size2 - i - 1)).data) {
                 break;
             }
         }
 
-        return path1.size() + path2.size() - i * 2;
+        return size1 + size2 - i * 2;
     }
 
-    private static int dist(boolean[][] edges, int n1, int n2) {
-        List<Integer> path1 = new ArrayList<>();
-        visited = new boolean[edges.length+1];
-        visited[1] = true;
-        getPath(path1, edges, 1, n1);
-        List<Integer> path2 = new ArrayList<>();
-        visited = new boolean[edges.length+1];
-        visited[1] = true;
-        getPath(path2, edges, 1, n2);
-
-        return getDist(path1, path2);
+    private static List<Node> getReversePath(Node n) {
+        List<Node> path = new ArrayList<>();
+        do {
+            path.add(n);
+            n = n.other;
+        } while (n != null);
+        return path;
     }
 
-    public static void main(String[] args) {
+    private static Map<Integer, List<Node>> getPaths(int[] nodeSet) {
+        Map<Integer, List<Node>> paths = new HashMap<>();
+        if (nodes.length > 1) {
+            for (int n = 0; n < nodeSet.length; n++) {
+                if (!paths.containsKey(nodeSet[n])) {
+                    paths.put(nodeSet[n], getReversePath(nodes[nodeSet[n]]));
+                }
+            }
+        }
+        return paths;
+    }
+
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
@@ -73,10 +69,22 @@ public class KittysCalculationsOnATree {
             }
         }
 
+        nodes = new Node[n+1];
+        for (int i = 1; i <= n; i++) {
+            nodes[i] = new Node(i);
+        }
+
         for (int i = 0; i < n-1; i++) {
             int n1 = sc.nextInt();
             int n2 = sc.nextInt();
-            edges[n1][n2] = true;
+            if (nodes[n2].other != null) { // n2 already has parent
+                if (nodes[n1].other != null) {
+                    throw new Exception("This is not a tree.");
+                }
+                nodes[n1].other = nodes[n2];
+            } else {
+                nodes[n2].other = nodes[n1];
+            }
         }
 
         for (int i = 0; i < q; i++) {
@@ -86,20 +94,12 @@ public class KittysCalculationsOnATree {
                 nodeSet[j] = sc.nextInt();
             }
 
-            int total = 0;
-            Arrays.sort(nodeSet);
-            Map<Integer, List<Integer>> paths = new HashMap<>();
-            for (int n1 = 0; n1 < nodeSet.length; n1++) {
-                List<Integer> path = new ArrayList<>();
-                visited = new boolean[edges.length+1];
-                getPath(path, edges, 1, nodeSet[n1]);
-                paths.put(nodeSet[n1], path);
-            }
+            long total = 0;
+            Map<Integer, List<Node>> paths = getPaths(nodeSet);
 
             for (int n1 = 0; n1 < nodeSet.length - 1; n1++) {
                 for (int n2 = n1 + 1; n2 < nodeSet.length; n2++) {
-                    //int distance = dist(edges, nodeSet[n1], nodeSet[n2]);
-                    total += ((nodeSet[n1] * nodeSet[n2] * getDist(paths.get(nodeSet[n1]), paths.get(nodeSet[n2]))) % (1000000007));
+                    total += (((long) nodeSet[n1] * (long) nodeSet[n2] * getDist(paths.get(nodeSet[n1]), paths.get(nodeSet[n2]))) % (1000000007L));
                 }
             }
             System.out.println(total);
